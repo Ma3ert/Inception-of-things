8c8ada831617f4e37e30ee4efcebf6ad25ea43dd
Ma3ert <yahyaitiazza15@gmail.com>
2024-08-14 01:15:38 +0100

    the installation kubctl installation also no tested

--- p1/tools/installServerWorker.sh
+++ p1/tools/installServerWorker.sh
@@ -1,2 +1,9 @@
+# Updating and install dependencies packages
 apt-get update -y && apt-get install curl
+
+# Installation of k3s in server mode
 curl -sfL https://get.k3s.io | K3S_TOKEN=xxx K3S_URL=https://server-url:6443 sh -
+
+# Installation of kubctl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
