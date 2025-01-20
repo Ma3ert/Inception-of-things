17f86425e1a7c22db4c5547d6e289433ee0c79e0
Anas Adnane <aadnane@e2r4p15.1337.ma>
2025-01-20 18:02:45 +0100

    returning to old main

---
+++ README.md
@@ -0,0 +1,2 @@
+# inception-of-things
+This project aims to deepen our knowledge in the devops fundamentals, using k3s/k3d along side with vagrant.
---
+++ p1/scripts/installServer.sh
@@ -0,0 +1,18 @@
+# Updating and install dependencies packages
+apt-get update -y
+
+# Installation of k3s in server mode
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-external-ip "192.168.56.111" --disable=traefik" sh -
+
+# Extract the token from the server to the shared folder
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant_data/node-token
+
+# Set k3s the configuration file for kubectl 
+mkdir -p /home/vagrant/.kube
+chmod 700 /home/vagra/.kube
+chown vagrant:vagrant /home/vagrant/.kube
+sudo cat /etc/rancher/k3s/k3s.yaml > /home/vagrant/.kube/config
+
+# Installation of kubctl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
